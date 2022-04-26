e5e2c1b1a5c671863435694f37f5a6e4296f6f19
AleksandrSl <sl.aleksandr28@gmail.com>
2022-04-26 21:40:16 +0300

    1 file modified

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <img alt="Vue logo" src="https://vuejs.org/images/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>sssssssslllжжжжллллддддддддд
-</template>ььддд
+</template>ььддддддддд
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
